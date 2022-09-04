07eb832801e27d2b254d181843b76a9e7a8552dd
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-09-04 11:58:43 -0400

    Update output

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ def home():
 
 @app.route("/pm2")
 def pm2():
-    return subprocess.run(["pm2", "logs", "--json", "--timestamp", "--nostream", "--lines", "50"], capture_output=True).stdout
+
+    with open('../.pm2/pm2.log', 'rb') as f:
+        try:  # catch OSError in case of a one line file
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b'\n':
+                f.seek(-2, os.SEEK_CUR)
+        except OSError:
+            f.seek(0)
+        last_line = f.readline().decode()
+    return last_line
+    # return subprocess.run(["pm2", "logs", "--json", "--timestamp", "--nostream", "--lines", "50"], capture_output=True).stdout
 
 def main():
     app.run("0.0.0.0")
