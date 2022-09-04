921617a1b930dad9df7e61dc0e12e8251b8acd3c
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-09-04 14:44:13 -0400

    Update output

--- app.py
+++ app.py
@@ -10,20 +10,9 @@ def home():
 
 @app.route("/pm2")
 def pm2():
-    num_newlines = 0
-    length = 50
-    lines = []
-    with open("../.pm2/pm2.log", 'rb') as f:
-        try:
-            f.seek(-2, os.SEEK_END)
-            while num_newlines < length:
-                f.seek(-2, os.SEEK_CUR)
-                if f.read(1) == b'\n':
-                    num_newlines += 1
-                    lines.append(f.readline().decode())
-        except OSError:
-            f.seek(0)
-        last_line = f.readline().decode()
+    n = 50
+    proc = subprocess.Popen(['tail', '-n', n, "../.pm2/pm2.log"], stdout=subprocess.PIPE)
+    lines = proc.stdout.readlines()
     return lines
     # return subprocess.run(["pm2", "logs", "--json", "--timestamp", "--nostream", "--lines", "50"], capture_output=True).stdout
 
