bc2157d64bff4c92eb769015bf5fa84cc17ad23b
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-09-04 14:45:06 -0400

    Update output

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
 @app.route("/pm2")
 def pm2():
     n = 50
-    proc = subprocess.Popen(['tail', '-n', n, "../.pm2/pm2.log"], stdout=subprocess.PIPE)
+    proc = subprocess.Popen(['tail', '-n', str(n), "../.pm2/pm2.log"], stdout=subprocess.PIPE)
     lines = proc.stdout.readlines()
     return lines
     # return subprocess.run(["pm2", "logs", "--json", "--timestamp", "--nostream", "--lines", "50"], capture_output=True).stdout
