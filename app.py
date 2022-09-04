cca5374ff538ca3b728e94fb642c6faca97ac7b8
Danny Ullrich <42869122+ullrichd21@users.noreply.github.com>
2022-09-04 12:04:57 -0400

    Update output

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ def home():
 
 @app.route("/pm2")
 def pm2():
+    num_newlines = 0
     length = 50
-    with open('../.pm2/pm2.log', 'rb') as f:
-        try:  # catch OSError in case of a one line file
+    with open("../.pm2/pm2.log", 'rb') as f:
+        try:
             f.seek(-2, os.SEEK_END)
-            while f.read(1) != b'\n':
-                f.seek(-2- length, os.SEEK_CUR)
+            while num_newlines < length:
+                f.seek(-2, os.SEEK_CUR)
+                if f.read(1) == b'\n':
+                    num_newlines += 1
         except OSError:
             f.seek(0)
         last_line = f.readline().decode()
